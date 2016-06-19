a191e0bcddcfd93558762a595d559e0467e9aba7
Dukho Choi <dukhochoi@gmail.com>
2016-06-19 20:57:00 +1000

    MapView fragment defined to separate map handling methods

---
+++ app/src/main/java/com/nomadworks/devtest/screens/scenario2/MapViewFragment.java
@@ -0,0 +1,96 @@
+package com.nomadworks.devtest.screens.scenario2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.nomadworks.devtest.R;
+
+import butterknife.Bind;
+
+/**
+ * Created by choidukho on 19/06/2016.
+ */
+public abstract class MapViewFragment extends Fragment {
+    @Bind(R.id.mapView)
+    protected MapView mapView;
+
+    protected GoogleMap mMap;
+    protected static final float DEFAULT_ZOOM = 12f;
+
+    private static LatLng DEFAULT_MAP_LOCATION = new LatLng(-33.861523,151.210884);
+
+    protected void initMap(@Nullable Bundle savedInstanceState) {
+        mapView.onCreate(savedInstanceState);
+        if (mMap != null) {
+            setUpMap();
+        } else {
+            mapView.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(final GoogleMap googleMap) {
+                    mMap = googleMap;
+                    if (mMap != null) {
+                        setUpMap();
+                    }
+                }
+            });
+        }
+    }
+
+    private void setUpMap() {
+        mMap.getUiSettings().setCompassEnabled(false);
+        setMapLocation(DEFAULT_MAP_LOCATION, false);
+    }
+
+    protected void setMapLocation(LatLng latLng, boolean animate) {
+        final CameraPosition.Builder builder = CameraPosition.builder()
+                .target(latLng)
+                .zoom(DEFAULT_ZOOM);
+        final CameraUpdate camera = CameraUpdateFactory.newCameraPosition(builder.build());
+
+        if(animate) {
+            mMap.animateCamera(camera);
+        } else {
+            mMap.moveCamera(camera);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mapView != null) {
+            mapView.onDestroy();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mapView != null) {
+            mapView.onSaveInstanceState(outState);
+        }
+    }
+}
--- app/src/main/java/com/nomadworks/devtest/screens/scenario2/Scenario2Fragment.java
+++ app/src/main/java/com/nomadworks/devtest/screens/scenario2/Scenario2Fragment.java
@@ -2,7 +2,6 @@ package com.nomadworks.devtest.screens.scenario2;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +13,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.nomadworks.devtest.R;
-import com.nomadworks.devtest.api.GetPlaceInfoApi;
 import com.nomadworks.devtest.model.PlaceInfo;
 import com.nomadworks.devtest.provider.ServiceProvider;
 import com.nomadworks.devtest.repository.DataRepository;
@@ -36,7 +28,7 @@ import butterknife.ButterKnife;
 /**
  * Created by choidukho on 19/06/2016.
  */
-public class Scenario2Fragment extends Fragment implements Scenario2Contract.ViewListener{
+public class Scenario2Fragment extends MapViewFragment implements Scenario2Contract.ViewListener{
     @Bind(R.id.spinnerPlace)
     Spinner spinnerPlace;
 
@@ -49,16 +41,8 @@ public class Scenario2Fragment extends Fragment implements Scenario2Contract.Vie
     @Bind(R.id.btnNavigate)
     Button btnNavigate;
 
-    @Bind(R.id.mapView)
-    MapView mapView;
-
-    GetPlaceInfoApi mApi;
     DataRepository mRepository;
     Scenario2Contract.Presenter mPresenter;
-
-    //map related
-    GoogleMap mMap;
-    private static final float DEFAULT_ZOOM = 12f;
 
     //view data
     private List<PlaceInfo> viewListPlace;
@@ -142,27 +126,6 @@ public class Scenario2Fragment extends Fragment implements Scenario2Contract.Vie
         });
     }
 
-    private void initMap(@Nullable Bundle savedInstanceState) {
-        mapView.onCreate(savedInstanceState);
-        if (mMap != null) {
-            setUpMap();
-        } else {
-            mapView.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(final GoogleMap googleMap) {
-                    mMap = googleMap;
-                    if (mMap != null) {
-                        setUpMap();
-                    }
-                }
-            });
-        }
-    }
-
-    private void setUpMap() {
-        mMap.getUiSettings().setCompassEnabled(false);
-    }
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -172,33 +135,21 @@ public class Scenario2Fragment extends Fragment implements Scenario2Contract.Vie
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (mapView != null) {
-            mapView.onDestroy();
-        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if (mapView != null) {
-            mapView.onPause();
-        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if (mapView != null) {
-            mapView.onResume();
-        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (mapView != null) {
-            mapView.onSaveInstanceState(outState);
-        }
     }
 
 
@@ -212,19 +163,6 @@ public class Scenario2Fragment extends Fragment implements Scenario2Contract.Vie
         LatLng latLng = new LatLng(placeInfo.getLocation().getLatitude(),
                 placeInfo.getLocation().getLongitude());
         setMapLocation(latLng, true);
-    }
-
-    private void setMapLocation(LatLng latLng, boolean animate) {
-        final CameraPosition.Builder builder = CameraPosition.builder()
-                .target(latLng)
-                .zoom(DEFAULT_ZOOM);
-        final CameraUpdate camera = CameraUpdateFactory.newCameraPosition(builder.build());
-
-        if(animate) {
-            mMap.animateCamera(camera);
-        } else {
-            mMap.moveCamera(camera);
-        }
     }
 
     private static final String TAB = "    ";
